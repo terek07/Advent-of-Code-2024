3381f782f177d0a63b598183d07729b04c08fb6c
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2024-12-01 20:40:07 +0100

    Add files via upload

---
+++ day1/main.py
@@ -0,0 +1,15 @@
+with open("input.txt","r") as file:
+    data = file.readlines()
+    print(data)
+    left = []
+    right = []
+    for e in data:
+        leftLine = e[:5]
+        rightLine = e[8:-1]
+        left.append(leftLine)
+        right.append(rightLine)
+    print(sum([int(e) * right.count(e) for e in left]))   #task 2 solution
+    left.sort()
+    right.sort()
+    print(sum([abs(int(l) - int(r)) for l, r in zip(left, right)]))   #task 1 solution
+
