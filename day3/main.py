cbf3db53afd1989048854ff2d797f34882d6002f
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2024-12-03 18:47:43 +0100

    day 3

--- day3/main.py
+++ day3/main.py
@@ -1,6 +1,5 @@
 import re
 
-
 with open("input.txt", "r") as file:
     data = file.read()
     correct = re.findall(r"mul\(\d{1,3},\d{1,3}\)", data) #finds all  mul(x, y)
@@ -11,3 +10,17 @@ with open("input.txt", "r") as file:
         y = int(e[i+1:-1])
         result.append(x * y) #multiplies x * y
     print(sum(result)) #task 1 solution
+    
+    
+    splited = data.split("do()")
+    dos = []
+    result = []
+    for e in splited:
+        dos = e.split("don't()")[0] #everything between do() and don't()
+        correct = re.findall(r"mul\(\d{1,3},\d{1,3}\)", dos)  # finds all  mul(x, y)
+        for el in correct:
+            i = el.index(",")
+            x = int(el[4:i])
+            y = int(el[i + 1:-1])
+            result.append(x * y)
+    print(sum(result)) #task 2 solution
