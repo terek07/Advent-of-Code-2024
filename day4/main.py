86cbfde512eed72596d64a37b5adf4a906755acb
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2024-12-04 13:59:10 +0100

    day 4

--- day4/main.py
+++ day4/main.py
@@ -1,9 +1,11 @@
+#task: https://adventofcode.com/2024/day/4
 with open("input.txt", "r") as file:
     data = [line.strip() for line in file.readlines()]
     counter = 0
+    counter2 = 0
     for i, e in enumerate(data):
-        for j, c in enumerate(e):
-            if c == "X":
+        for j, c in enumerate(e): 
+            if c == "X":# finds XMAS
                 try:
                     if e[j + 1] == "M" and e[j + 2] == "A" and e[j + 3] == "S":
                         counter += 1
@@ -24,7 +26,7 @@ with open("input.txt", "r") as file:
                         counter += 1
                 except IndexError:
                     pass
-            if c == "S":
+            if c == "S": #finds SAMX
                 try:
                     if e[j + 1] == "A" and e[j + 2] == "M" and e[j + 3] == "X":
                         counter += 1
@@ -45,4 +47,27 @@ with open("input.txt", "r") as file:
                         counter += 1
                 except IndexError:
                     pass
-    print(counter)
+            if c == "A": # finds X-MAS(2 MASes in shape of x)
+                try:
+                    if i >= 1 and j >= 1 and data[i + 1][j + 1] == "M" and data[i + 1][j - 1] == "M" and data[i - 1][j + 1] == "S" and data[i - 1][j - 1] == "S":
+                        counter2 += 1
+                except IndexError:
+                    pass
+                try:
+                    if i >= 1 and j >= 1 and data[i + 1][j + 1] == "M" and data[i + 1][j - 1] == "S" and data[i - 1][j + 1] == "M" and data[i - 1][j - 1] == "S":
+                        counter2 += 1
+                except IndexError:
+                    pass
+                try:
+                    if i >= 1 and j >= 1 and data[i + 1][j + 1] == "S" and data[i + 1][j - 1] == "M" and data[i - 1][j + 1] == "S" and data[i - 1][j - 1] == "M":
+                        counter2 += 1
+                except IndexError:
+                    pass
+                try:
+                    if i >= 1 and j >= 1 and data[i + 1][j + 1] == "S" and data[i + 1][j - 1] == "S" and data[i - 1][j + 1] == "M" and data[i - 1][j - 1] == "M":
+                        counter2 += 1
+                except IndexError:
+                    pass
+                
+    print(counter) #task 1 solution
+    print(counter2) #task 2 solution
