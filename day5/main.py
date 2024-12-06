2505884db8a1a5286be475e53be4ac95329d4fcf
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2024-12-06 16:27:19 +0100

    day5 task1

--- day5/main.py
+++ day5/main.py
@@ -6,18 +6,21 @@ left = [e[0 : 2] for e in rules]
 right = [e[3 : 5] for e in rules]
 result = []
 for line in data:
+    line = line.split(",")
     correct = True
     for l, r in zip(left, right):
         if l in line and r in line:
-            for i, char in enumerate(line.split(",")):
-                try:
-                    if line.index(r) > i:
-                        correct = False
-                        break
-                except ValueError:
-                    pass
+            if line.index(l) >= line.index(r):
+                    correct = False
+                    break
+
     if correct:
-        result.append(line)
-print(rules)
+        print(line)
+        print(line[len(line) // 2 + 1])
+        result.append(int(line[len(line) // 2]))
+# print(rules)
+print(left)
+# print(right)
 print(data)
 print(result)
+print(sum(result))
